fc3fd16d52caea76b8241cdcfd8a1d05c42698d2
Tincho Develops <97058453+tinchopapa56@users.noreply.github.com>
2022-09-18 12:07:16 -0300

    tutorial all build (cant deploy, bugs)

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x4EF7c0B9fb74Bf6148C68bA06979a1C82649670E"; //DEPLOYED im RINJKEBY
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL= "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const ALCHEMY_API = process.env.ALCHEMY_API;
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks:{
+    // rinkeby: {
+    //   url: ALCHEMY_API_KEY_URL,
+    //   accounts: [RINKEBY_PRIVATE_KEY],
+    // },
+    goerli: {
+      url: ALCHEMY_API,
+      accounts: [GOERLI_PRIVATE_KEY],
+    }
+  }
 };
